b861225a08fb974ec38dc542d3a7f2d9aa63e0dc
Giarrusso Gianluca <43678653+alreadyrussian@users.noreply.github.com>
2020-01-03 18:15:47 +0100

    Update add-theme-support.php

--- digitalism/includes/add-theme-support.php
+++ digitalism/includes/add-theme-support.php
@@ -36,7 +36,7 @@ if ( ! function_exists( 'digitalism_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'digitalism' ),
+			'primary' => esc_html__( 'Primary', 'digitalism' ),
 		) );
 
 		/*
